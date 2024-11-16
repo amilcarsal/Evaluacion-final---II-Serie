93da772007e7a72b5c55911f87b87988ca15239a
amilcarsal <135087213+amilcarsal@users.noreply.github.com>
2024-11-16 08:04:26 -0600

    Ver 1.0

---
+++ tsconfig.ts
@@ -0,0 +1,9 @@
+{
+    "compilerOptions": {
+      "baseUrl": ".",          // Define la base de búsqueda para los módulos
+      "paths": {
+        "@/lib/*": ["lib/*"]    // Configura el alias @/lib/ para que apunte a la carpeta lib/
+      }
+    }
+  }
+  
